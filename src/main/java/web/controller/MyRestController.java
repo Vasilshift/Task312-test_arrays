428d5d6b70665a270f4dcb75efbbda4a6097c6e3
Evgeny <vasilkalin@gmail.com>
2021-08-20 21:35:13 +0200

    Add annotation Jackson

---
+++ src/main/java/web/controller/MyRestController.java
@@ -0,0 +1,33 @@
+package web.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import web.model.User;
+import web.service.RoleService;
+import web.service.UserService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class MyRestController {
+
+    private final UserService userService;
+    private final RoleService roleService;
+
+    @Autowired
+    public MyRestController(UserService userService, RoleService roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
+    }
+
+    @GetMapping("/users")
+    public List<User> showAllUsers() {
+        List<User> allUsers = userService.findAll();
+        return allUsers;
+    }
+
+}
